4e5915772a2850e51ea3a3ed73872aacd7f5e108
16750145927 <2104819695@qq.com>
2022-03-29 21:57:19 +0800

    疫情防控管理系统

--- teacher/con.php
+++ teacher/con.php
@@ -9,6 +9,27 @@ $class = $_POST['class'];
 $pass = $_POST['pass'];
 
 
+
+//判断数据库里面的用户名也没有相同的有的用户名，有就执行插入数据库的操作，反之提示用户名存在！
+$sqla = "SELECT user FROM teacher";
+
+$resulta = $conn->query($sqla);
+
+if (mysqli_num_rows($resulta) > 0) {
+    // 输出数据
+    while($rowa = mysqli_fetch_assoc($resulta)) {
+
+        if ($rowa['user'] == $user ){
+            echo "<script>alert('抱歉已存在用户名！！');window.location.assign('regteacher.php')</script>";
+
+        }
+
+    }
+}
+
+
+
+
 //获取数据
 $sql = "INSERT INTO `teacher`(`id`,`user`,`name`,`school`,`xibu`,`class`,`pass`)
 VALUES (null,'$user','$name','$school','$xibu','$class','$pass')";
