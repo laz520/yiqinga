fc2d135cdd91e40f08b170155e06db488a15431a
16750145927 <2104819695@qq.com>
2022-03-30 13:22:47 +0800

    疫情防控管理系统

---
+++ admin/set1.php
@@ -0,0 +1,26 @@
+<?php
+include ("../conn.php");
+
+
+
+$title = $_POST['title'];
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+$qq = $_POST['qq'];
+$gg = $_POST['gg'];
+$gg1 = $_POST['gg1'];
+$gg2 = $_POST['gg2'];
+
+$id = 1;
+//mysqli修改语句
+$sql = "update admin set title='$title',user='$user',pass='$pass',qq='$qq',gg='$gg',gg1='$gg1',gg2='$gg2' where id='$id'";
+
+//执行sql语句
+$result=mysqli_query($conn,$sql);
+
+//判断执行成功的话就直接跳转ss1.php页面，反之提示无法修改数据，输出报错的原因
+if(!$result){
+    die('无法修改数据,:'.mysqli_error($conn));
+}else{
+    echo "<script>window.location.assign('ss1.php')</script>";
+}
